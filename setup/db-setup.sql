39c8dc683f7ea6be240a31a3d79e5c06072fb95e
Eric McKenna <eric.mckenna@gmail.com>
2012-09-28 18:46:24 -0400

    added task and attendence table

--- setup/db-setup.sql
+++ setup/db-setup.sql
@@ -8,5 +8,29 @@ CREATE TABLE `user` (
   `firstname` varchar(255) NOT NULL,
   `lastname` varchar(255) NOT NULL,
   PRIMARY KEY (`uid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Table structure for table `task`
+--
+CREATE TABLE `task` (
+  `tid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `start` datetime NOT NULL,
+  `end` datetime NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `recurrence` int(11) NOT NULL,
+  PRIMARY KEY (`tid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+--
+-- Table structure for table `attendence`
+--
+CREATE TABLE `attendence` (
+  `aid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `uid` int(10) unsigned NOT NULL,
+  `tid` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`aid`),
+  KEY `tid` (`tid`),
+  KEY `uid` (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
